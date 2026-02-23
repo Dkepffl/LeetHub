54dd4c38c33d9f3d2c0a3ef53f56a04ae06dbb4d
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2026-02-23 14:07:00 +0900

    Time: 1106 ms (20.17%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,4 @@
+SELECT R.contest_id, ROUND(COUNT(R.user_id)*100/(SELECT COUNT(DISTINCT user_id) FROM Users), 2) AS percentage
+FROM Register R JOIN Users U ON R.user_id=U.user_id
+GROUP BY R.contest_id
+ORDER BY percentage DESC, contest_id ASC
