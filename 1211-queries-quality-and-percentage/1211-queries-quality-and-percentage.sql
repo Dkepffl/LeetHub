578b94d0de3808d035c65708b9c0dfc30c7e338f
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2026-05-06 12:15:37 +0900

    Time: 387 ms (26.04%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,7 +1,6 @@
-SELECT query_name,
-       ROUND(AVG(rating/position),2) AS quality,
+SELECT query_name, 
+       ROUND(AVG(rating/position),2) as quality, 
        ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(*)*100, 2) 
-       AS poor_query_percentage # need to review
+       as poor_query_percentage
 FROM Queries
 GROUP BY query_name
-
