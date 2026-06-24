ae18b28a0de0cc8b60c82ad1f125b8f39466b4a8
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2026-06-24 14:25:23 +0900

    Time: 992 ms (5.46%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,10 @@
-WITH Manager AS(
-    SELECT *
-    FROM Employees
-)
-
-SELECT M.employee_id, M.name, COUNT(E.employee_id) AS reports_count, ROUND(AVG(E.age) ,0) AS average_age
-FROM Manager M JOIN Employees E ON M.employee_id = E.reports_to
+SELECT 
+    M.employee_id, 
+    M.name, 
+    COUNT(E.employee_id) AS reports_count, 
+    ROUND(AVG(E.age), 0) AS average_age 
+FROM Employees M
+JOIN Employees E ON M.employee_id = E.reports_to
 GROUP BY M.employee_id
-ORDER BY M.employee_id
+ORDER BY M.employee_id;
 
