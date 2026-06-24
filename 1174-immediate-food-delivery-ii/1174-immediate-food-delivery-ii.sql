38ed0c4a7665b05b7bf1188214d240af05ab9690
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2026-06-24 12:39:42 +0900

    Time: 642 ms (63.04%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,9 +1,25 @@
-SELECT ROUND(AVG(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END)*100, 2) AS immediate_percentage
-FROM (SELECT 
+WITH ranked_delivery AS (
+    SELECT 
         customer_id,
         order_date,
         customer_pref_delivery_date,
         -- 고객별로 주문 날짜가 빠른 순서대로 순위를 매김
         ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date) AS rn
-    FROM Delivery) ranked_delivery
-WHERE rn = 1
+    FROM Delivery
+), 
+ORDERSTATE AS (
+    SELECT 
+        customer_id, 
+        order_date,
+        customer_pref_delivery_date,
+        CASE WHEN order_date = customer_pref_delivery_date THEN 'immediate' ELSE 'scheduled' END AS STATE
+    FROM Delivery
+)
+
+SELECT 
+    ROUND(AVG(CASE WHEN STATE = 'immediate' THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM ranked_delivery r
+JOIN ORDERSTATE o 
+  ON r.customer_id = o.customer_id 
+ AND r.order_date = o.order_date
+WHERE r.rn = 1;
