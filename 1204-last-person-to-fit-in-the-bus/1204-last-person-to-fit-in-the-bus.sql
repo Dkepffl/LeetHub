e0339a7da332e3dcc7959b032c631d3916c37a08
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2026-07-07 14:09:18 +0900

    Time: 916 ms (38.42%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+WITH BoardingQueue AS (
+    SELECT 
+        person_name,
+        SUM(weight) OVER (ORDER BY turn ASC) AS total_weight
+    FROM Queue
+)
+
+SELECT person_name
+FROM BoardingQueue
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1
